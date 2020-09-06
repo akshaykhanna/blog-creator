c08fa63ec9d1cb4944c11bf8c1c5eb4f1cc09251
Akshay Khanna <akshaykhanna@Akshays-MacBook-Pro.local>
2020-09-06 13:36:06 +0530

    Change gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitleAlt: `T-Log`,
   },
   plugins: [
     {
@@ -45,13 +45,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `T-Log - Akshay Khanna`,
+        short_name: `T-Log`,
+        description: `Would be writing here tech blogs, some of my experiments & weird stuff !!!`,
         start_url: `/blog`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
         display: `standalone`,
+        author: `Akshay Khanna`,
         icons: [
           {
             src: `/android-chrome-192x192.png`,
